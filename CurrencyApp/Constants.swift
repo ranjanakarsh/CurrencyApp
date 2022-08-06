e6f1a2bfa01fa3c09bcff06f4ab70eb6f78ec356
Ranjan Akarsh <49028243+ranjanakarsh@users.noreply.github.com>
2022-08-06 14:31:03 +0530

    Add files via upload

--- CurrencyApp/Constants.swift
+++ CurrencyApp/Constants.swift
@@ -5,5 +5,5 @@
 //  Created by Ranjan Akarsh on 06/08/22.
 //
 
-let CURRENCY_API = "bPJPipadqXz69uRMHGLYs9VXMrBHdSRT"
+let CURRENCY_API = "bPJPiladqXz69uRMHGLYs9VTMrBHdSRT" // YOUR API KEY HERE
 let CURRENCY_URL = "https://api.apilayer.com/fixer/latest?apikey="
